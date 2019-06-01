749250bd010e400485dbb46b0127a21d357839b6
student39 <student39@pcoip.ki.agh.edu.pl>
2019-06-01 15:30:02 +0200

    main menu

--- src/main/java/pl/edu/agh/mwo/App.java
+++ src/main/java/pl/edu/agh/mwo/App.java
@@ -2,63 +2,19 @@ package pl.edu.agh.mwo;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class App {
 	
     public static void main(String[] args) throws IOException {
 
-		listAllFiles("C:\\Users\\Yogi\\Downloads\\reporter-dane\\reporter-dane");
+	  //listAllFiles("C:\\Users\\Yogi\\Downloads\\reporter-dane\\reporter-dane");
       
       String pathToData = args[0];
-
-        boolean isRunning = true;
-        Scanner in = new Scanner(System.in);
-
-        while (isRunning) {
-            System.out.println("=== Raporty ===");
-            System.out.println(pathToData);
-            System.out.println("1. Raport roczny (ogólny)");
-            System.out.println("2. Raport - Liczba godzin wypracowanych per projekt");
-            System.out.println("3. Raport - szczegółowy wykaz pracy pracownika");
-            System.out.println("4. Raport - procentowe zaangażowanie w projekty");
-            System.out.println("5. Raport - top 10 aktywności");
-            System.out.println("0 - wyjscie");
-
-            System.out.print(">>>");
-            int input = in.nextInt();
-
-            switch (input) {
-                case 1:
-                    System.out.println("Raport 1");
-                    System.out.print("Podaj rok: ");
-                    input = in.nextInt();
-                    System.out.println("rok: " + input);
-                    System.out.println("1. Wyświetl zawartość\n2. Wyeksportuj do Excela");
-
-                    break;
-                case 2:
-                    System.out.println("Raport 2");
-                    break;
-                case 3:
-                    System.out.println("Raport 3");
-                    break;
-                case 4:
-                    System.out.println("Raport 4");
-                    break;
-                case 5:
-                    System.out.println("Raport 5");
-                    break;
-            }
-
-            if (input == 0) {
-                isRunning = false;
-            }
-        }
+      
+      MainMenu.Run();
 
 	}
 	
@@ -78,5 +34,4 @@ public class App {
 		return listFiles;
     }
 	
-}
 }
