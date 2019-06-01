dcb70dc7a4a4780a9158a800cb927d2b6c9aaaec
unknown <juda.jablonska@gmail.com>
2019-06-01 12:56:43 +0200

    tree

--- src/main/java/pl/edu/agh/mwo/App.java
+++ src/main/java/pl/edu/agh/mwo/App.java
@@ -1,4 +1,19 @@
 package pl.edu.agh.mwo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class App {
+	
+	public static void main(String args[]) throws Exception {
+		//printTree("E:\\");
+	}
+	
+	public static void printTree(String path) throws IOException {
+		Files.walk(Paths.get(path))
+        .filter(Files::isRegularFile)
+        .forEach(System.out::println);
+	}
+	
 }
